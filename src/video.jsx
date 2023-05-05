38d2253146c59b9685dd4d274b3475b35fd02910
wiel <yahiesdev@gmail.com>
2023-05-05 15:58:58 -0500

    video save

--- src/video.jsx
+++ src/video.jsx
@@ -1,12 +1,27 @@
+import ReactPlayer from "react-player"
 
-export function Video({setOpenvideo}) {
-    return(
-    <div id="videoweb">
-        <div className="btn-video" onClick={()=>setOpenvideo(false)}>X</div>
-        <div class="videocontent spinner-border text-dark" role="status">
-            {/* <span class="visually-hidden">Loading...</span> */}
-           {/*  <div>cargando ..</div> */}
+
+export function Video({ setOpenvideo }) {
+    return (
+        <div id="videoweb">
+            <div className="centervideo">
+                <div className="btn-video" onClick={() => setOpenvideo(false)}>X</div>
+                <div className="videocontent">
+                    <ReactPlayer
+                        url={'https://www.youtube.com/watch?v=pLBuFxMYkx8'}
+                      /*     width="100%"
+                         height="100%"  */
+
+                        controls
+                        loop
+                        muted
+                        playing
+
+                    ></ReactPlayer>
+
+                </div>
+            </div>
+
         </div>
-    </div>
     )
 }
